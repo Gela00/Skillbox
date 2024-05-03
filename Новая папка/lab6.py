b10c1c4954906d296f10e0aa8e5d4661379b85f6
Gela00 <126690847+Gela00@users.noreply.github.com>
2024-05-03 08:03:24 +0500

    Update lab6.py

--- Новая папка/lab6.py
+++ Новая папка/lab6.py
@@ -1,21 +1,21 @@
 def min_difference(weights):
     # сумма всех весов
     total_weight = sum(weights)
-    # создание массива динамического программирования (вся сумма + 1) заполняет нулями
-    dp = [0] * (total_weight + 1)
+    # создание массива (вся сумма + 1) заполняет нулями
+    a = [0] * (total_weight + 1)
 
     # первый элемент делаем единицей
-    dp[0] = 1
+    a[0] = 1
 
     for weight in weights:
         for i in range(total_weight, -1, -1):
             # если вес был достигнут, устанавливается 1
-            if dp[i]:
-                dp[i + weight] = 1
+            if a[i]:
+                a[i + weight] = 1
 
     # Ищем наименьшую разницу, начиная с половины общего веса.
     for diff in range((total_weight + 1) // 2, total_weight + 1):
-        if dp[diff]:
+        if a[diff]:
             # возвращение минимальной разницы
             return diff - (total_weight - diff)
 
