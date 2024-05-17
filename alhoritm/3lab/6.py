b231a208159b06aea234e5f82cce379b1c0f7b02
Gela00 <126690847+Gela00@users.noreply.github.com>
2024-05-17 10:28:43 +0500

    Update 6.py

--- alhoritm/3lab/6.py
+++ alhoritm/3lab/6.py
@@ -21,3 +21,41 @@ def process_commands(commands):
 # Тестовые данные
 commands = [("+", "5"), ("+", "3"), ("?", ""), ("-", ""), ("?", ""), ("+", "2"), ("?", "")]
 print(process_commands(commands))  # Вывод: [3, 5, 2]
+
+
+class CustomQueue:
+    def __init__(self):
+        self.queue = []
+
+    def push(self, element):
+        self.queue.append(element)
+        self.queue.sort()
+
+    def pop(self):
+        if len(self.queue) > 0:
+            self.queue.pop(0)
+
+    def get_min(self):
+        if len(self.queue) > 0:
+            return self.queue[0]
+        return None
+
+def process_commands(commands):
+    queue = CustomQueue()
+    results = []
+
+    for command in commands:
+        if command[0] == "+":
+            queue.push(int(command[1]))
+        elif command[0] == "-":
+            queue.pop()
+        elif command[0] == "?":
+            min_element = queue.get_min()
+            if min_element is not None:
+                results.append(min_element)
+
+    return results
+
+# Тестовые данные
+commands = [("+", "5"), ("+", "3"), ("?", ""), ("-", ""), ("?", ""), ("+", "2"), ("?", "")]
+print(process_commands(commands))  # Вывод: [3, 5, 2]
