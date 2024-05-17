0d7753e61794111f6c8d5f8067a627eb6284d0bb
Gela00 <126690847+Gela00@users.noreply.github.com>
2024-05-17 11:10:45 +0500

    Update 17.py

--- alhoritm/3lab/17.py
+++ alhoritm/3lab/17.py
@@ -23,3 +23,40 @@ print(max_intersection_size(N1, sets1))
 print(max_intersection_size(N2, sets2))
 
 
+
+def intersection(set1, set2):
+    # Создание пустого списка для хранения пересечения
+    result = []
+
+    # Перебор всех элементов первого множества
+    for element in set1:
+        # Проверка, содержится ли элемент во втором множестве
+        if element in set2:
+            # Добавление элемента в список пересечения
+            result.append(element)
+
+    return result
+
+def max_intersection_size(N, sets):
+    # Инициализация переменной для хранения максимального размера пересечения
+    max_size = 0
+
+    # Перебор всех пар множеств
+    for i in range(N):
+        for j in range(i+1, N):
+            # Нахождение пересечения двух множеств
+            common_elements = intersection(sets[i], sets[j])
+            # Обновление максимального размера пересечения, если текущее пересечение больше
+            max_size = max(max_size, len(common_elements))
+
+    # Возвращаем максимальный размер пересечения
+    return max_size
+
+# Тестовые данные
+N1 = 4
+sets1 = [[-2, 6, 8, 4, -1], [5, 3, 10, -5, -1], [7, 8, -5, -1, -2], [-1, 8, 4, 9, 0]]
+
+N2 = 3
+sets2 = [[9, 7, 1, 8], [5, 7, 6, 3], [5, 9, 8, 6]]
+print(max_intersection_size(N1, sets1))
+print(max_intersection_size(N2, sets2))
